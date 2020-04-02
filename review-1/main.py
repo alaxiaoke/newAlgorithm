9afc4914614df303b490d4a205cb28560f90b228
alaxiaoke <l>
2020-04-02 17:08:00 +0800

    开源rnn改良

---
+++ review-1/main.py
@@ -0,0 +1,76 @@
+import os
+import pandas as pd
+import pprint
+
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+import tensorflow.contrib.slim as slim
+
+from data_model import StockDataSet
+from model_rnn import LstmRNN
+
+flags = tf.app.flags
+flags.DEFINE_integer("stock_count", 100, "Stock count [100]")
+flags.DEFINE_integer("input_size", 1, "Input size [1]")#input data sample
+flags.DEFINE_integer("num_steps", 30, "Num of steps [30]")
+flags.DEFINE_integer("num_layers", 1, "Num of layer [1]")
+flags.DEFINE_integer("lstm_size", 128, "Size of one LSTM cell [128]")
+flags.DEFINE_integer("batch_size", 64, "The size of batch images [64]")
+flags.DEFINE_float("keep_prob", 0.8, "Keep probability of dropout layer. [0.8]")
+flags.DEFINE_float("init_learning_rate", 0.001, "Initial learning rate at early stage. [0.001]")
+flags.DEFINE_float("learning_rate_decay", 0.99, "Decay rate of learning rate. [0.99]")
+flags.DEFINE_integer("init_epoch", 5, "Num. of epoches considered as early stage. [5]")
+flags.DEFINE_integer("max_epoch", 50, "Total training epoches. [50]")
+flags.DEFINE_string("stock_symbol", None, "Target stock symbol [None]")
+flags.DEFINE_integer("sample_size", 4, "Number of stocks to plot during training. [4]")
+flags.DEFINE_boolean("train", False, "True for training, False for testing [False]")
+
+FLAGS = flags.FLAGS
+
+pp = pprint.PrettyPrinter()
+
+if not os.path.exists("logs"):
+    os.mkdir("logs")
+
+def load_sp500(input_size, num_steps, k=None, target_symbol=None, test_ratio=0.05):
+    if target_symbol is not None:
+        return [
+            StockDataSet(
+                target_symbol,
+                input_size=input_size,
+                num_steps=num_steps,
+                test_ratio=test_ratio)
+        ]
+
+def main(_):
+    pp.pprint(FLAGS.__flags)
+    run_config = tf.ConfigProto()
+    run_config.gpu_options.allow_growth = True
+
+    with tf.Session(config=run_config) as sess:
+        rnn_model = LstmRNN(
+            sess,
+            FLAGS.stock_count,
+            lstm_size=FLAGS.lstm_size,
+            num_layers=FLAGS.num_layers,
+            num_steps=FLAGS.num_steps,
+            input_size=FLAGS.input_size,
+            keep_prob=FLAGS.keep_prob,
+        )
+        stock_data_list = load_sp500(
+            FLAGS.input_size,
+            FLAGS.num_steps,
+            k=FLAGS.stock_count,
+            target_symbol=FLAGS.stock_symbol,
+        )
+
+        if FLAGS.train:
+            rnn_model.train(stock_data_list, FLAGS)
+        else:
+            if not rnn_model.load()[0]:
+                raise Exception("[!] Train a model first, then run test mode")
+
+
+if __name__ == '__main__':
+    tf.app.run()
